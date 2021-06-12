b7a1d6de9b3670c4382aa1fc25e2b2115077b158
Pranav Vashi <neobuddy89@gmail.com>
2021-06-12 10:54:02 +0530

    sm8350-common: Inherit gapps config if exists
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I6e07df91644998fa874dffa3341f19d85b0f351a

--- common.mk
+++ common.mk
@@ -32,6 +32,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sm8350-common/sm8350-common-vendor.mk)
 
+# Google Basic apps
+$(call inherit-product-if-exists, vendor/gapps/basic/config.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
