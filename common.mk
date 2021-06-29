591fe79a847d8f0fc81f22d6802772c22c81d152
Pranav Vashi <neobuddy89@gmail.com>
2021-06-29 20:48:26 +0530

    sm8350-common: Remove OnePlus Dolby for now
    * This causes loud audio issue even at volume level < 20%.
    
    Revert "sm8350-common: Update Dolby Atmos"
    
    This reverts commit ecf72ba162c6b83353aeab4e8ad1f520d692c649.
    
    Revert "sm8350-common: Hide Dolby Atmos from launcher"
    
    This reverts commit 5bfb648e6995456e56f9128497e75952cd6e4964.
    
    Revert "sm8350-common: Add OnePlus Dolby"
    
    This reverts commit 8aaf44731634a27c2f98d72efe4c64aa70549e9a.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -367,7 +367,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs_performance_lahaina.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_lahaina.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance_lahaina_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_lahaina_vendor.xml \
     $(LOCAL_PATH)/configs/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml \
-    $(LOCAL_PATH)/configs/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_profiles_lahaina.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_lahaina.xml \
     $(LOCAL_PATH)/configs/media_profiles_lahaina.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
