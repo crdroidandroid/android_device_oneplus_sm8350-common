d07083fcf10d0e273b0c8203ca3753ba96a92461
Pranav Vashi <neobuddy89@gmail.com>
2021-07-09 23:06:49 +0530

    sm8350-common: Update BT blobs from LA.UM.9.14.r1-16100-LAHAINA.0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -187,7 +187,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
