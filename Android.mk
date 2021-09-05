875a2bffb70ddcf294becd2a4bd68276490a29c6
Andrzej Perczak <kartapolska@gmail.com>
2021-09-05 17:35:52 +0800

    sm8350-common: Symlink libvndfwk_detect_jni.qti for CneApp
    Needed by CneApp.
    
    dlopen failed: library "/system_ext/lib64/libvndfwk_detect_jni.qti.so" needed or dlopened by "/apex/com.android.art/lib64/libnativeloader.so" is not accessible for the namespace "vendor-classloader-namespace"
    
    Change-Id: I400f03c3cb60acdc4263c24b1096c5961142f03f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- Android.mk
+++ Android.mk
@@ -106,4 +106,14 @@ $(WIFI_FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	$(hide) ln -sf /odm/vendor/etc/wifi/WCNSS_qcom_cfg.ini $@/wlan/qca_cld/WCNSS_qcom_cfg.ini
 
 ALL_DEFAULT_INSTALLED_MODULES += $(ACDBDATA_SYMLINKS) $(RFS_MSM_ADSP_SYMLINKS) $(RFS_MSM_CDSP_SYMLINKS) $(RFS_MSM_MPSS_SYMLINKS) $(RFS_MSM_SLPI_SYMLINKS) $(RFS_MSM_WPSS_SYMLINKS) $(WIFI_FIRMWARE_SYMLINKS)
+
+CNE_LIBS := libvndfwk_detect_jni.qti.so
+CNE_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_APPS)/CneApp/lib/arm64/,$(notdir $(CNE_LIBS)))
+$(CNE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "CNE lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /vendor/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(CNE_SYMLINKS)
 endif
