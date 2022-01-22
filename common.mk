d0e405532f779ff9700902c33d31cab83300df83
Alexander Koskovich <akoskovich@pm.me>
2022-01-22 00:46:40 -0700

    sm8350-common: Update telephony package list to LA.QSSI.12.0.r1-05600-qssi.0.
    Change-Id: Id6fdabd9d5e0df6d23a982e0e39e06a8acff67ef
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -472,10 +472,18 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     ims-ext-common \
     ims_ext_common.xml \
+    extphonelib \
+    extphonelib-product \
+    extphonelib.xml \
+    extphonelib_product.xml \
     qti-telephony-hidl-wrapper \
+    qti-telephony-hidl-wrapper-prd \
     qti_telephony_hidl_wrapper.xml \
+    qti_telephony_hidl_wrapper_prd.xml \
     qti-telephony-utils \
+    qti-telephony-utils-prd \
     qti_telephony_utils.xml \
+    qti_telephony_utils_prd.xml \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
