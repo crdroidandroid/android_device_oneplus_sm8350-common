9d41bd4eaa2175bfc3affbc1d61a4bfa3c34aa19
Pranav Vashi <neobuddy89@gmail.com>
2022-02-24 01:20:27 +0530

    sm8350-common: Enable UDFPS icons and animations
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -181,6 +181,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+TARGET_HAS_UDFPS := true
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
