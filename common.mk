f0a4779b65921d8220abfb5c928bc34764106d73
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2022-06-11 02:02:20 +0000

    sm8350-common: Enable freeform windows

--- common.mk
+++ common.mk
@@ -183,6 +183,10 @@ PRODUCT_COPY_FILES += \
 
 TARGET_HAS_UDFPS := true
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
