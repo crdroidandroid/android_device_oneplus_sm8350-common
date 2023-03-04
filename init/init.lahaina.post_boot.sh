25999ea386d21798f139749122372bcbbae7265c
Wei Wang <wvw@google.com>
2023-03-05 00:50:04 +0600

    sm8350-common: Boot time tuning
    Test: build and boottime test
    Change-Id: Ib8f31d39501b4747f0c268d2dd87e814bcc7843b
    Signed-off-by: Wei Wang <wvw@google.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- init/init.lahaina.post_boot.sh
+++ init/init.lahaina.post_boot.sh
@@ -23,3 +23,17 @@ echo 128 > /sys/block/dm-9/queue/read_ahead_kb
 # cpuset
 echo 0-1 > /dev/cpuset/background/cpus
 echo 0-3 > /dev/cpuset/system-background/cpus
+
+# Setup runtime schedTune
+echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+echo "schedutil" > /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor
+echo 1 > /dev/stune/foreground/schedtune.prefer_idle
+echo 0 > /dev/stune/foreground/schedtune.prefer_high_cap
+echo 0 > /dev/stune/foreground/schedtune.boost
+echo 0 > /dev/stune/schedtune.prefer_idle
+echo 0 > /dev/stune/schedtune.prefer_high_cap
+echo 0 > /dev/stune/schedtune.boost
+echo 1 > /dev/stune/top-app/schedtune.prefer_idle
+echo 0 > /dev/stune/top-app/schedtune.prefer_high_cap
+echo 10 > /dev/stune/top-app/schedtune.boost 10
