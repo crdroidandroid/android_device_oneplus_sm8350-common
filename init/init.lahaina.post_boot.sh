5dbb615a80c6654f5433e2d5cdd2c3e4701b17cc
Chenyang Zhong <zhongcy95@gmail.com>
2023-03-07 06:39:37 +0000

    sm8350-common: init: Avoid migrating foreground tasks to X1
    The Cortex-X1 core is quite power hungry. Let's only use it for
    top-app tasks. Avoid migrating foreground tasks to X1 to reduce
    unnecessary bumps of X1's frequency and save some battery.
    
    Change-Id: I8bb7655010da98c083560d647d5c682cd636cc82
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- init/init.lahaina.post_boot.sh
+++ init/init.lahaina.post_boot.sh
@@ -23,6 +23,7 @@ echo 128 > /sys/block/dm-9/queue/read_ahead_kb
 # cpuset
 echo 0-1 > /dev/cpuset/background/cpus
 echo 0-3 > /dev/cpuset/system-background/cpus
+echo 0-6 > /dev/cpuset/foreground/cpus
 
 # Setup runtime schedTune
 echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
