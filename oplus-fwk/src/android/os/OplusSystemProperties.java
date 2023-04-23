316c792f82e21e6af895c33be067020e45e7cd77
Chandu Dyavanapelli <chandudyavanapelli03@gmail.com>
2023-04-23 19:06:51 +0530

    sm8350-common: Update oplus-fwk
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- oplus-fwk/src/android/os/OplusSystemProperties.java
+++ oplus-fwk/src/android/os/OplusSystemProperties.java
@@ -2,6 +2,14 @@ package android.os;
 
 public class OplusSystemProperties {
 
+    public static String get(String key) {
+        return SystemProperties.get(key);
+    }
+
+    public static String get(String key, String def) {
+        return SystemProperties.get(key, def);
+    }
+
     public static int getInt(String key, int def) {
         return SystemProperties.getInt(key, def);
     }
--- oplus-fwk/src/com/oplus/os/OplusBuild.java
+++ oplus-fwk/src/com/oplus/os/OplusBuild.java
@@ -6,4 +6,10 @@ public class OplusBuild {
     public static int getOplusOSVERSION() {
         return 23;
     }
+
+    public static class VERSION {
+        public static final String RELEASE = "unknown";
+        public static final int SDK_VERSION = getOplusOSVERSION();
+        public static final int SDK_SUB_VERSION = 1;
+    }
 }
