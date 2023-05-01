20efe2dd1663a97f587ba03d9ad27be4ed361a77
pjgowtham <pjgowtham@gmail.com>
2023-05-01 09:14:54 +0530

    sm8350-common: Build oplus camera

--- common.mk
+++ common.mk
@@ -110,6 +110,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-service
 
 # Camera
+$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
+
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
