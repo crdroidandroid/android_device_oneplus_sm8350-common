a65ac1f27691b43fe26454b1ba72257a0166cfd1
Pranav Vashi <neobuddy89@gmail.com>
2023-05-06 20:14:10 +0530

    sm8350-common: Move few kernel scripts to device tree
    * The oplus modifications are no-op. It's better to have working
      qcom implementation instead.
    * This also allows to further fine-tune.

--- common.mk
+++ common.mk
@@ -178,6 +178,8 @@ PRODUCT_PACKAGES += \
     fstab.default \
     fstab.default.vendor_ramdisk \
     init.class_main.sh \
+    init.kernel.post_boot-lahaina.sh \
+    init.kernel.post_boot-yupik.sh \
     init.oplus.hw.rc \
     init.oplus.hw.rc.recovery \
     init.oplus.rc \
@@ -188,6 +190,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    init.qti.kernel.rc \
     init.target.rc \
     ueventd.oplus.rc \
     ueventd.qcom.rc
