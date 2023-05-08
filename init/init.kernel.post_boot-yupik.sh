03d10fd70909200472501b9804c20b4fba519c14
Pranav Vashi <neobuddy89@gmail.com>
2023-05-09 00:49:24 +0530

    sm8350-common: Retrict cpuset for non-critical groups

--- init/init.kernel.post_boot-yupik.sh
+++ init/init.kernel.post_boot-yupik.sh
@@ -167,8 +167,10 @@ echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 echo 0 > /proc/sys/kernel/sched_coloc_busy_hysteresis_enable_cpus
 
 # cpuset parameters
-echo 0-3 > /dev/cpuset/background/cpus
+echo 0-1 > /dev/cpuset/background/cpus
+echo 0-3 > /dev/cpuset/restricted/cpus
 echo 0-3 > /dev/cpuset/system-background/cpus
+echo 0-6 > /dev/cpuset/foreground/cpus
 
 # Turn off scheduler boost at the end
 echo 0 > /proc/sys/kernel/sched_boost
