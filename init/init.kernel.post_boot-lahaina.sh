5ae04e0e38b4a14ba2c292d2aa6ab89d848902e7
Pranav Vashi <neobuddy89@gmail.com>
2023-05-09 00:49:24 +0530

    sm8350-common: Retrict cpuset for non-critical groups

--- init/init.kernel.post_boot-lahaina.sh
+++ init/init.kernel.post_boot-lahaina.sh
@@ -163,8 +163,10 @@ echo 325 > /proc/sys/kernel/walt_low_latency_task_threshold
 echo 162 > /proc/sys/kernel/sched_min_task_util_for_colocation
 
 # cpuset parameters
-echo 0-3 > /dev/cpuset/background/cpus
+echo 0-1 > /dev/cpuset/background/cpus
+echo 0-3 > /dev/cpuset/restricted/cpus
 echo 0-3 > /dev/cpuset/system-background/cpus
+echo 0-6 > /dev/cpuset/foreground/cpus
 
 # Turn off scheduler boost at the end
 echo 0 > /proc/sys/kernel/sched_boost
