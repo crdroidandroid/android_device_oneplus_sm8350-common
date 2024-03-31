a8ede3662a7996fe586149346ca4ab207e9cabff
pjgowtham <pjgowtham@gmail.com>
2024-03-31 14:33:55 +0530

    sm8350-common: Switch to oplus 2.1 sensors hal

--- common.mk
+++ common.mk
@@ -341,7 +341,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.1-service.oplus-multihal \
     libsensorndkbridge \
     sensors.oplus
 
