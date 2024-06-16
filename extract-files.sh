c4824141400cfcf58c57cac3643cb4d1324ffc02
Michael Bestas <mkbestas@lineageos.org>
2024-06-16 18:07:13 +0300

    sm8350-common: Add missing libprocessgroup dependency
    Change-Id: I079293410dd98d78fafbca837480f80328570346

--- extract-files.sh
+++ extract-files.sh
@@ -66,6 +66,8 @@ function blob_fixup() {
     case "${1}" in
         odm/bin/hw/android.hardware.ir-service.oplus)
             "${PATCHELF}" --replace-needed "android.hardware.ir-V1-ndk_platform.so" "android.hardware.ir-V1-ndk.so" "${2}"
+        odm/bin/hw/vendor.pixelworks.hardware.display.iris-service)
+            grep -q "libprocessgroup.so" "${2}" || "${PATCHELF}" --add-needed "libprocessgroup.so" "${2}"
             ;;
         odm/etc/camera/CameraHWConfiguration.config)
             sed -i "/SystemCamera = / s/1;/0;/g" "${2}"
