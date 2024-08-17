ee68b92a49cbbe43a26e03551e2531e5ceb85e69
SX <neoroms69@gmail.com>
2024-08-17 19:30:12 +0530

    sm8350-common: Decommonize lunaa specific things
    *since ice has Lcd instead of Amoled and with no udfps
    
    Revert "sm8350-common: Enable UDFPS icons and animations"
    
    This reverts commit 170d2a876dbf1dc4bc3643c07d91c3cf1e8051ce.
    
    Revert "sm8350-common: Enable screen off udpfs support"
    
    This reverts commit c146c7ce769574dba49e4f672748c0d595e6eb2d.
    
    Revert "sm8350-common: enable smart pixels"
    
    This reverts commit 29ef9365800be5874273d31903fdc5ec3a871dbd.

--- common.mk
+++ common.mk
@@ -200,12 +200,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
-TARGET_HAS_UDFPS := true
-
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
-
+    
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
