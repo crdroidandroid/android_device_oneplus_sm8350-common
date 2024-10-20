176fdfaa3b69dd7b5ee6a1c3fe58bb484f2efd84
Lup Gabriel <gwolf2u.website@gmail.com>
2024-10-20 17:23:59 +0300

    TEMP: fun time

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -96,6 +96,7 @@ BOARD_KERNEL_CMDLINE := \
     swiotlb=0 \
     ip6table_raw.raw_before_defrag=1 \
     iptable_raw.raw_before_defrag=1
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
