7ed46be1c005aa4d31509fcfeb110ec5bc9a2753
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-10-23 19:14:26 +0300

    sm8350-common: Drop soundtrigger HIDL impl  * breaks google voice match with pixelpropsutils as    we don't have hotword enrollment support

--- common.mk
+++ common.mk
@@ -41,7 +41,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
-    android.hardware.soundtrigger@2.3-impl \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.usb.default \
