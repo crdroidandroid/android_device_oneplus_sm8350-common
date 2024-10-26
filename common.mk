50dc4e8552266f8275865cb4a541ac8de0c23edb
LuK1337 <priv.luk@gmail.com>
2024-10-26 08:34:56 +0200

    sm8350-common: Build 64-bit only libvndfwk_detect_jni.qti.vendor
    symlink_target: "/vendor/lib64/libvndfwk_detect_jni.qti.so".
    
    Change-Id: I651706330ff88bda8920ec0a8670ebe1a434acac

--- common.mk
+++ common.mk
@@ -245,7 +245,7 @@ PRODUCT_PACKAGES += \
 
 # QTI fwk-detect
 PRODUCT_PACKAGES += \
-    libvndfwk_detect_jni.qti.vendor # Needed by CNE app
+    libvndfwk_detect_jni.qti.vendor:64 # Needed by CNE app
 
 # Sensors
 PRODUCT_PACKAGES += \
