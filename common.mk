bf7af7457eaaa427bc4d193d73a54084624bffd8
LuK1337 <priv.luk@gmail.com>
2025-01-20 08:14:32 +0200

    sm8350-common: Build gralloc.qcom

--- common.mk
+++ common.mk
@@ -110,6 +110,7 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
+    gralloc.qcom \
     init.qti.display_boot.rc \
     init.qti.display_boot.sh \
     vendor.qti.hardware.display.allocator-service \
