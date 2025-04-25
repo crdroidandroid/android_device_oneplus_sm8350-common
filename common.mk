1e1dc86104aec943ba836b43c31de515f2513404
Bruno Martins <bgcngm@gmail.com>
2025-04-25 17:36:15 +0100

    sm8350-common: Switch to Touch AIDL
    Change-Id: I92a19fedb9ea482616c2e3d550e30b69d2a3a5c7

--- common.mk
+++ common.mk
@@ -323,7 +323,7 @@ PRODUCT_PACKAGES += \
 
 # Touch
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.oplus
+    vendor.lineage.touch-service.oplus
 
 # Update engine
 PRODUCT_PACKAGES += \
