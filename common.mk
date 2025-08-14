9f72444cbcaf0f5a7eb81a2de6c218a9f73247b4
LuK1337 <priv.luk@gmail.com>
2025-08-14 23:47:18 +0200

    sm8350-common: Migrate to AIDL LiveDisplay HAL
    Change-Id: I4e8be836e94d442b40c63a1ac7dade3c9d9c784c

--- common.mk
+++ common.mk
@@ -209,7 +209,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.oplus
+    vendor.lineage.livedisplay-service.oplus
 
 # Media
 PRODUCT_COPY_FILES += \
