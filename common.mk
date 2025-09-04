45fde475b63adbd5d486f457d011ad76eb9e2083
Lup Gabriel <gwolf2u.website@gmail.com>
2025-09-04 21:24:20 +0300

    sm8350-common: enable blur

--- common.mk
+++ common.mk
@@ -395,5 +395,8 @@ PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
     firmware_WCNSS_qcom_cfg.ini_symlink
 
+# Blur
+TARGET_ENABLE_BLUR := true
+
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/sm8350-common/sm8350-common-vendor.mk)
